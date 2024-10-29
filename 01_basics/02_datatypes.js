9b9aabe181bd09a683f8bd1bd2a43621e03fa1d8
Saksham Maurya <162309232+saksham3322@users.noreply.github.com>
2024-10-29 16:32:23 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict" //treat all JS code as newer version
+
+//alert(3+1) // we are using nodejs, not browser
+
+console.log(3+3) //code readability should be high
+console.log("Saksham")
+
+let age = 18 
+let name = "Saksham"
+let isLoggedIn = false
+
+//number => 2 to the power 53
+//bigint
+// boolean => true/false
+// string => ""
+// null => standalone value
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
