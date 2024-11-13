5e77dc6cede8fb0237d109c7ab0958aef9ba97a1
Saksham Maurya <162309232+saksham3322@users.noreply.github.com>
2024-11-13 13:27:41 +0000

    objects method done

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,56 @@
+// const tinderUser = new Object()
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "saksham"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userFullName: {
+            firstName: "saksham",
+            lastName: "maurya"
+        }
+    }
+}
+
+// console.log(regularUser.fullname);
+// console.log(regularUser.fullname.userFullName.firstName);
+// console.log(regularUser.fullname.userFullName.lastName);
+
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "c", 4: "d"}
+const obj3 = {5: "e", 6: "f"}
+
+// const obj4 = {obj1, obj2, obj3}
+// const obj4 = Object.assign({}, obj1, obj2, obj3)
+const obj4 = {...obj1, ...obj2, ...obj3}
+console.log(obj4);
+
+const users = [
+    {
+        id: 1,
+        email: "u1@gmail.com"
+    },
+    {
+        id: 2,
+        email: "u2@gmail.com"
+    },
+    {
+        id: 3,
+        email: "u3@gmail.com"
+    },
+]
+console.log(users);
+console.log(users[1]);
+console.log(users[2].email);
