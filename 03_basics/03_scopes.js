614a87b3c6d00ed9b64a50ada787e416fd69bc85
Saksham Maurya <162309232+saksham3322@users.noreply.github.com>
2024-11-18 16:08:08 +0000

    scope level & hoisting basics

--- 03_basics/03_scopes.js
+++ 03_basics/03_scopes.js
@@ -1,14 +1,55 @@
 var c = 300 // -> bad practice
-console.log(c);
+// console.log(c);
 
 let a = 300
 if(true) {
     let a = 30
     const b = 20
     var c = 40
-    console.log("Inner 'a':",a);
+    // console.log("Inner 'a':",a);
 }
 
-console.log("Outer 'a':", a);
+// console.log("Outer 'a':", a);
 // console.log(b); -> this line gives error bcz of scope level
-console.log(c);
+// console.log(c);
+
+function one() {
+    const username = "saksham"
+
+    function two() {
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website); // -> this gives error
+    two()
+}
+
+// one()
+
+if(true) {
+    const username = "saksham"
+    if(username === "saksham") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website); // -> this gives error
+}
+
+// console.log(username); // -> this gives error
+
+// +++++++++++++++++ Interesting +++++++++++++++++
+// ++++++++++++ Hoisting Basics ++++++++++++++++++
+
+console.log(addOne(5));
+
+function addOne(num) {
+    return num + 1
+}
+
+// addTwo(4) // -> this gives error bcz used before initialization
+
+const addTwo = function(num) {
+    return num + 2
+}
+
+console.log(addTwo(8));
