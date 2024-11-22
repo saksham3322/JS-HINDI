41040cd6b2a4c15fca06a7e87b5c2239ffc614bc
Saksham Maurya <162309232+saksham3322@users.noreply.github.com>
2024-11-22 16:18:59 +0000

    switch case done

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,19 @@
+const month = 2 // we can also use string values
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    default:
+        console.log("default case");
+        break;
+}
