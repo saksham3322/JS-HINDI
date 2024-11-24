247056f3ab8a39d1f269803482a1f58ebfc161d0
Saksham Maurya <162309232+saksham3322@users.noreply.github.com>
2024-11-24 04:57:45 +0000

    do while loop

---
+++ 05_iterations/03_do_while_loop.js
@@ -0,0 +1,14 @@
+let score = 1
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
+console.log();
+
+let index = 11
+
+do {
+    console.log(`Index is: ${index}`);
+    index++
+} while (index <= 10);
