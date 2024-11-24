924e6d8ce59ea34309849c9442b83a92c377e697
Saksham Maurya <162309232+saksham3322@users.noreply.github.com>
2024-11-24 04:54:48 +0000

    while loop

---
+++ 05_iterations/02_while_loop.js
@@ -0,0 +1,15 @@
+let index = 0
+
+while(index <= 10) {
+    console.log(`Value of index is ${index}`);
+    index += 2
+}
+
+let myArray = ["superman", "batman", "flash"]
+
+let arr = 0
+
+while(arr < myArray.length) {
+    console.log(`Value at index ${arr} is: ${myArray[arr]}`);
+    arr++;
+}
