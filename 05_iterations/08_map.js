38194553bce3f9afe1cb81a6f3c0b10b8e3e071d
Saksham Maurya <162309232+saksham3322@users.noreply.github.com>
2024-11-26 07:26:28 +0000

    map property

---
+++ 05_iterations/08_map.js
@@ -0,0 +1,11 @@
+const myNum = [1, 2, 3, 4, 5]
+
+let newNum = myNum.map( (num) => num + 10)
+
+console.log(newNum);
+
+newNum = myNum.map((num) => num * 10)
+              .map((num) => num + 1)
+              .filter((num) => num >= 30)
+
+console.log(newNum);
