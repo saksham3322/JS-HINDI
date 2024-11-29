71c260f3b8de5ee241f79f5e96b626d74dafb650
Saksham Maurya <162309232+saksham3322@users.noreply.github.com>
2024-11-29 07:13:52 +0000

    reduce method

---
+++ 05_iterations/09_reduce.js
@@ -0,0 +1,35 @@
+const myNums = [1, 2, 3]
+
+const myTotal = myNums.reduce(function (acc, curr) {
+    // console.log(`acc: ${acc} and curr: ${curr}`);
+    return acc + curr
+}, 0)
+
+// console.log(myTotal);
+
+const myTotalSum = myNums.reduce( (acc, curr) => acc + curr, 0)
+
+// console.log(myTotalSum);
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "python course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+const priceToPay = shoppingCart.reduce( (acc, item) => acc + item.price, 0)
+
+console.log(priceToPay);
